ee8af6bb7bf46031b4584f4507ea4bd629971455
longcat99 <33887488+longcat99@users.noreply.github.com>
2020-03-18 06:54:55 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,5 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.240/g' package/base-files/files/bin/config_generate
+src-git lienol https://github.com/Lienol/openwrt-package
