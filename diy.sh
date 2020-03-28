9e4f367eecc225be44011023a8ad9988f7523ac0
longcat99 <33887488+longcat99@users.noreply.github.com>
2020-03-27 23:34:15 -0500

    1

--- diy.sh
+++ diy.sh
@@ -9,3 +9,5 @@
 sed -i 's/192.168.1.1/192.168.100.240/g' package/base-files/files/bin/config_generate
 #src-git lienol https://github.com/Lienol/openwrt-package
 git clone https://github.com/vernesong/OpenClash                                 package/clash
+git clone https://github.com/rufengsuixing/luci-app-adguardhome                  package/adguardhome
+ 
