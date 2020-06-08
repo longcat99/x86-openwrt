470041926bf14f2ff757a86f0ac0d73c36424eff
longcat99 <33887488+longcat99@users.noreply.github.com>
2020-06-09 01:45:46 +0800

    y

--- diy.sh
+++ diy.sh
@@ -11,7 +11,7 @@ sed -i 's/192.168.1.1/192.168.100.240/g' package/base-files/files/bin/config_gen
 git clone https://github.com/vernesong/OpenClash                                 package/clash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome                  package/adguardhome
 git clone https://github.com/tzxiaozhen88/koolproxyR                         package/koolproxyR
-git clone https://github.com/ElonH/Rclone-OpenWrt                 package/rclone
+# git clone https://github.com/ElonH/Rclone-OpenWrt                 package/rclone
 git clone https://github.com/frainzy1477/luci-app-clash           package/luci-app-clash
 git clone https://github.com/jerrykuku/luci-theme-argon           package/luci-theme-argon
 # git clone https://github.com/longcat99/opdiy/tree/master/diy/luci-app-passwall                      package/luci-app-passwall
